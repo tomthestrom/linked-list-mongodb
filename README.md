235f48a42277487b5ffbb39ebcea9905f3ef6560
tomthestrom <thruska29@gmail.com>
2021-02-19 16:14:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Linked list & Doubly linked list & MongoDB
+
+Initial code inspired by [Joe Karlsson](https://twitter.com/JoeKarlsson1)'s youtube video [@MongoDB](https://www.youtube.com/channel/UCK_m2976Yvbx-TyDLw7n1WA) youtube channel - [Intro to Linked Lists with MongoDB](https://www.youtube.com/watch?v=JCMx5SkhNVE).
+
+I decided to build upon the code written in the video and create a Doubly Linked List DS as a basis to implementing a functionality within my hobby project.
+The initial code is a direct copy of the code written in the video, except for the db connection string, db name and collection name being extracted into a *.env* file as a security measure.
---
+++ app.js
@@ -0,0 +1,126 @@
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const atlasUri = process.env.ATLAS_URI;
+const atlasDBName = process.env.ATLAS_DB_NAME;
+const atlasDBCollection = process.env.ATLAS_DB_COLLECTION;
+class LinkedList {
+    async init () {
+
+        const uri = atlasUri;
+        this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        try {
+            await this.client.connect();
+            console.log('connected to mongo db atlas');
+            this.collection = this.client.db(atlasDBName).collection(atlasDBCollection);
+        }
+         catch (err) {
+             console.error(err.stack);
+         }
+    }
+
+    async resetAtlasData () {
+        await this.collection.deleteMany({value: { $exists: true}});
+    }
+
+    async resetMeta() {
+        await this.collection.updateOne(
+            {meta: true},
+            { $set: { head: null, tail: null }},
+            { upsert: true }
+        )
+    }
+    async createNewNode (value) {
+        const newNode = await this.collection.insertOne({ value, next: null});
+        return newNode;
+    }
+
+    async getMeta () {
+        const meta = await this.collection.find({meta: true}).next();
+
+        return meta;
+    }
+
+    async getHead () {
+        const meta =  await this.getMeta();
+        return meta.head;
+    }
+
+    async setHead (newNodeID) {
+        await this.collection.updateOne(
+            {meta: true},
+            { $set: { head: newNodeID}}
+        )
+    }
+    async setTail (newNodeID) {
+        await this.collection.updateOne(
+            {meta: true},
+            { $set: { tail: newNodeID}}
+        )
+    }
+    async getTail (newNodeID) {
+        const meta =  await this.getMeta();
+        return meta.tail;
+        
+    }
+    async add(value) {
+        const newNode = await this.createNewNode(value);
+        const newNodeID = newNode.insertedId;
+        const head = await this.getHead();
+
+        //adding a node to an empty linked list
+        if (head === null) {
+            this.setHead(newNodeID);
+            this.setTail(newNodeID);
+        } else {
+            //adding a node to a non empty linked list
+            const tailID = await this.getTail();
+            await this.collection.updateOne(
+                {_id: tailID},
+                { $set: { next: newNodeID } }
+            );
+        }
+
+        this.setTail(newNodeID);
+    }
+
+    async get(index) {
+        if (index <= -1) {
+            throw new Error('invalid index');
+        }
+
+        const head = await this.getHead();
+        let currentNode = await this.collection.find({_id: head}).next();
+        console.log(currentNode);
+
+        let position = 0;
+        //loop through the nodes till we hit the index
+        while (position < index) {
+            if (currentNode.next === null) {
+                throw new Error('index overflow');
+            }
+            currentNode = await this.collection.find({_id: currentNode.next}).next();
+            position++;
+        }
+
+        return currentNode;
+    }
+
+    
+}
+
+(async function () {
+    try {
+       const linkedList = new LinkedList();
+       await linkedList.init(); 
+       await linkedList.resetAtlasData()
+       await linkedList.resetMeta()
+       await linkedList.add('cat');
+       await linkedList.add('dog');
+       await linkedList.add('turtle');
+       const result = await linkedList.get(1);
+        console.log(result)
+    } catch (error) {
+        console.error(error.stack);
+    }
+})()
+
